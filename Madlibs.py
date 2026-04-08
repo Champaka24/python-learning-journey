98daf6bda21082877754ca28cdff84ab80df931d
chrao <chrao.learning.gmail.com>
2026-04-08 15:01:22 +0530

    Madlibs

---
+++ Madlibs.py
@@ -0,0 +1,14 @@
+#Madlibs is a word game where you can create a story by filling blanks with random words
+
+
+adjective1=input("enter an adjective (description of something): ")
+noun1=input("Enter a noun (a place, person, thing): ")
+adjective2=input("enter an adjective (description): ")
+verb1=input("Enter a verb ending with 'ing'): ")
+adjective3=input("enter an adjective (description of something): ")
+
+
+print(f"Today I went to a {adjective1} zoo.")
+print(f"In an exibit, I saw a {noun1}.")
+print(f"{noun1} was {adjective2} and {verb1}")
+print(f"I was {adjective3}")
