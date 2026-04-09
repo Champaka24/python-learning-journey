2ed32eb434e3adf35b37909ec691babb2b027937
Champaka24 <krchampaka23@gmail.com>
2026-04-09 20:58:45 +0530

    Arithmetic

---
+++ arithmetic.py
@@ -0,0 +1,18 @@
+import math #importing the math module to access mathematical functions and constants
+
+x=23
+# print(math.pi)
+# print(math)
+# print(math.e)
+
+result=math.sqrt(x)#square root of x
+print(result)
+
+result1= math.sqrt(25) #square root of 25
+print(result1)
+
+result2=math.ceil(3.5) #rounds up to the nearest integer
+print(result2)
+
+result3=math.floor(4.8) #rounds down to the nearest integer
+print(result3)
