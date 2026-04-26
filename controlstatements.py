cca3e0a4bffe40d6c76774d9c0128f5da2cafcb9
Champaka24 <krchampaka23@gmail.com>
2026-04-27 01:45:19 +0530

    controlstatements

---
+++ controlstatements.py
@@ -0,0 +1,9 @@
+#1.if= do something only if some conditions is true.
+# else do something else
+
+age= int(input("Enter your age: "))
+
+if age<18:
+    print("youy're not allowed for voting")
+else:
+    print("you're allowed to vote")
